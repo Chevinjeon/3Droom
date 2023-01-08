ad39e908bcfebede886e9756f31b150cc233c6ce
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2023-01-08 16:57:46 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import "./styles.css";
+import React, { useState } from "react";
+import styled from "styled-components";
+import * as TextStyles from "./TextStyles";
+
+import Logo from "./components/Logo";
+import PlayButton from "./components/PlayButton";
+import Header from "./components/Header";
+import SplineScene from "./components/SplineScene";
+import CloseButton from "./components/CloseButton";
+
+import ImageControl from "./images/controls.png";
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Wrapper className="App">
+      <LogoContainer>
+        <Logo />
+      </LogoContainer>
+      <ContentWrapper>
+        <Container>
+          <TextContainer>
+            <Title>Hi there! </Title>
+            <Description>
+              {" "}
+              I'm a first-year Computer Science Student at the University of
+              Waterloo currently seeking Summer 2023 co-op. I love working with
+              software, artificial intelligence and computer algorithms. My
+              interests include exploring neuroscience, visualizing the art of
+              the emotion, and soft plushies have my whole heart.{" "}
+            </Description>
+          </TextContainer>
+          <ButtonContainer>
+            <PlayButton onClick={() => setIsOpen(true)} />
+          </ButtonContainer>
+          <GradientLine />
+          <ControlContainer1>
+            <Subtitle>Keyboard Key</Subtitle>
+            <ControlImage src={ImageControl} alt="control image" />
+          </ControlContainer1>
+        </Container>
+        <SplineContainer isOpen={isOpen}>
+          <Header />
+          <CloseButton isOpen={isOpen} onClick={() => setIsOpen(false)} />
+          <SplineScene />
+        </SplineContainer>
+      </ContentWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  overflow: hidden;
+`;
+
+const LogoContainer = styled.div`
+  position: absolute;
+  top: 44px;
+  left: 0px;
+`;
+
+const TextContainer = styled.div`
+  display: grid;
+  gap: 24px;
+  max-width: 460px;
+  padding: 225px 0px 0px 56px;
+`;
+
+const Title = styled(TextStyles.H1)``;
+
+const Description = styled.p`
+  max-width: 360px;
+  text-align: left;
+  font-family: "Inter", BlinkMacSystemFont, "Segoe UI", -apple-system,
+    BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell,
+    "Open Sans", "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 21px;
+  color: rgba (0, 0, 0, 0.6);
+  margin: 0;
+`;
+
+const ButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: start;
+  gap: 20px;
+  background: linear-gradient()
+    (270deg, #f1f1f1 12.86%, rgba(255, 255, 255, 0) 83.72%);
+  mix-blend-mode: normal;
+  padding: 40px 0px 40px 56px;
+  margin-top: 53px;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const GradientLine = styled.div`
+  width: 100%;
+  height: 48px;
+  background: linear-gradient(
+    185deg,
+    rgba(0, 0, 0, 0.2) 0%,
+    rgba(255, 255, 255, 0) 50%
+  );
+
+  @media (max-width: 1400px) {
+    display: none;
+  }
+`;
+
+const ControlContainer1 = styled.div`
+  display: block;
+
+  @media (max-width: 1400px) {
+    display: none;
+  }
+`;
+
+const Subtitle = styled(TextStyles.BodyMain)`
+  color: rgba(0, 0, 0, 0.6);
+  margin-top: 14px;
+  padding-left: 56px;
+`;
+
+const ControlImage = styled.img`
+  max-width: 326px;
+  padding: 14px 0px 0px 56px;
+`;
+
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 855px;
+  max-width: 1440px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media (max-width: 1400px) {
+    grid-template-columns: 1fr;
+    align-content: center;
+    padding-bottom: 40px;
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const SplineContainer = styled.div`
+  position: ${(props) => (props.isOpen ? "fixed" : "relative")};
+  padding: ${(props) => (props.isOpen ? "0px" : "24px 20px 24px 0px")};
+
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: auto;
+
+  .spline {
+    border-radius: ${(props) => (props.isOpen ? "0px" : "20px")};
+  }
+
+  .closeButton {
+    top: 54%;
+    left: -32px;
+  }
+  @media (max-width: 1400px) {
+    padding: ${(props) => (props.isOpen ? "0px" : "0px 20px 10px")};
+
+    .closeButton {
+      top: -44px;
+      left: 50%;
+      margin-left: -50px;
+      transform: rotate(90deg);
+    }
+  }
+`;
